02734d4c5b8d69b0d24a6cbe8473694c1847fcf1
Jayesh Gajbhar <fathom-jgajbhar@Mac-1885.lan>
2026-01-21 12:59:18 -0500

    Mod

--- config/site.ts
+++ config/site.ts
@@ -4,22 +4,18 @@ export const siteConfig = {
   name: "FischCast QC Dashboard",
   description: "A quality control dashboard for FischCast project data.",
   navItems: [
-    {
-      label: "Regions",
-      href: "/regions",
-    },
+    // {
+    //   label: "Regions",
+    //   href: "/regions",
+    // },
   ],
   navMenuItems: [
-    {
-      label: "Regions",
-      href: "/regions",
-    },
+    // {
+    //   label: "Regions",
+    //   href: "/regions",
+    // },
   ],
   links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    fathomscience: "https://fathomscience.com",
   },
 };
